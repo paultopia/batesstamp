b36033c363da91f568191aa8086055bcc3caf1cf
Paul Gowder <paul.gowder@gmail.com>
2017-08-28 21:46:51 -0500

    can paginate

--- src/main/java/BatesStamp.java
+++ src/main/java/BatesStamp.java
@@ -15,12 +15,13 @@ import org.apache.pdfbox.util.Matrix;
 // based on http://svn.apache.org/repos/asf/pdfbox/trunk/examples/src/main/java/org/apache/pdfbox/examples/pdmodel/AddMessageToEachPage.java
 
 public class BatesStamp {
-	private static float fontSize = 36.0f;
+	private static float fontSize = 12.0f;
 	private static PDFont font = PDType1Font.HELVETICA_BOLD;
 
-    private static void addToPage(PDDocument doc, PDPage page, float stringWidth, String message) throws IOException {
+    private static void addToPage(PDDocument doc, PDPage page, String pg) throws IOException {
 	    PDRectangle pageSize = page.getMediaBox();
 	    int rotation = page.getRotation();
+      float stringWidth = font.getStringWidth(pg)*fontSize/1000f;
 	    boolean rotate = rotation == 90 || rotation == 270;
 	    float pageWidth = rotate ? pageSize.getHeight() : pageSize.getWidth();
 	    float pageHeight = rotate ? pageSize.getWidth() : pageSize.getHeight();
@@ -34,20 +35,20 @@ public class BatesStamp {
             	contentStream.setTextMatrix(Matrix.getRotateInstance(Math.PI / 2, centerX, centerY));
             else
             	contentStream.setTextMatrix(Matrix.getTranslateInstance(centerX, centerY));
-            contentStream.showText(message);
+            contentStream.showText(pg);
             contentStream.endText();
         }
 	}
 
     public static void main(String[] args) throws IOException {
         String file = args[0];
-        String message = args[1];
-        String outfile = args[2];
-        float stringWidth = font.getStringWidth(message)*fontSize/1000f;
+        String outfile = args[1];
+        int pagenum = 1;
         try (PDDocument doc = PDDocument.load(new File(file))) {
             for (PDPage page : doc.getPages()){
-        	addToPage(doc, page, stringWidth, message);
-        	}
+                String pg = Integer.toString(pagenum++); // reminder to self: post-eval black magic java trick https://stackoverflow.com/questions/2315705/what-is-the-difference-between-i-i-in-for-loop-java
+                addToPage(doc, page, pg);
+            }
         doc.save(outfile);
         }
     }
